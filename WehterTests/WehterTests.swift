7c5ab6d8390581315e50c64da866a706e138098e
yobbo_wang <yobbo_wang@yobbo-wangdemac.local>
2017-11-30 14:51:12 +0800

    Initial Commit

---
+++ WehterTests/WehterTests.swift
@@ -0,0 +1,36 @@
+//
+//  WehterTests.swift
+//  WehterTests
+//
+//  Created by yobbo_wang on 17/11/30.
+//  Copyright © 2017年 yobbo_wang. All rights reserved.
+//
+
+import XCTest
+@testable import Wehter
+
+class WehterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
